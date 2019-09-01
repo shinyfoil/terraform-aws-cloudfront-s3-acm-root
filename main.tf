2ff69cc3e2bfdaa81a05779c76a70d172ae7b4b6
robc-io <robc.io.opensource@gmail.com>
2019-09-01 01:01:19 -0700

    adding subj alt name

--- main.tf
+++ main.tf
@@ -57,9 +57,9 @@ resource "aws_acm_certificate" "certificate" {
   //  domain_name = ".${var.root_domain_name}"
   domain_name = "${var.root_domain_name}"
   validation_method = "DNS"
-//  TODO: Logic to include wildcard
-//  subject_alternative_names = [
-//    "*.${var.root_domain_name}"]
+//  TODO: Validate
+  subject_alternative_names = [
+    "*.${var.root_domain_name}"]
 
   lifecycle {
     create_before_destroy = true
